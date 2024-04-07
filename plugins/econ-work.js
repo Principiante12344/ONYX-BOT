085f993e001bb73033ad3cf66a7cb48331501634
Kennn <111826360+Principiante12344@users.noreply.github.com>
2024-04-07 14:20:42 -0500

    Update econ-work.js

--- plugins/econ-work.js
+++ plugins/econ-work.js
@@ -1,42 +1,31 @@
+import axios from 'axios';
+import { msToTime, pickRandom } from './utils'; // Asumiendo que tienes un archivo utils.js con las funciones msToTime y pickRandom
 
-import fetch from 'node-fetch'
-import axios from 'axios'
-let cooldown = 3600000
+let cooldown = 3600000;
 let handler = async (m, { conn, usedPrefix, command }) => {
+    let hasil = Math.floor(Math.random() * 500);
+    let user = global.db.data.users[m.sender];
+    if (new Date() - user.lastwork < cooldown) {
+        throw `🧘🏻‍♂️ ${mssg.workCd} *${msToTime((user.lastwork + cooldown) - new Date())}*`;
+    }
 
-  let hasil = Math.floor(Math.random() * 500)
-  let user = global.db.data.users[m.sender]
-  if (new Date - user.lastwork < cooldown) throw `🧘🏻‍♂️ ${mssg.workCd} *${msToTime((user.lastwork + cooldown) - new Date())}*`
-
-    /*let w = await axios.get(global.API('fgmods', '/api/work', { }, 'apikey'))
-    let res = w.data.result*/
-    let anu = (await axios.get('https://raw.githubusercontent.com/fgmods/fg-team/main/games/work.json')).data
-    let res = pickRandom(anu)
- user.coin += hasil
-
-  m.reply(`
+    try {
+        let anu = (await axios.get('https://raw.githubusercontent.com/fgmods/fg-team/main/games/work.json')).data;
+        let res = pickRandom(anu);
+        user.coin += hasil;
+        
+        m.reply(`
 ‣ ${res.fgwork} *${hasil} 🪙*
-`)
-  user.lastwork = new Date * 1
-}
-handler.help = ['work']
-handler.tags = ['econ']
-handler.command = ['work', 'w', 'trabajar']
+`);
+        user.lastwork = new Date() * 1;
+    } catch (err) {
+        console.error(err);
+        throw 'Ocurrió un error al obtener la información del trabajo.';
+    }
+};
+handler.help = ['work'];
+handler.tags = ['econ'];
+handler.command = ['work', 'w', 'trabajar'];
 
-export default handler
-
-function msToTime(duration) {
-  var milliseconds = parseInt((duration % 1000) / 100),
-    seconds = Math.floor((duration / 1000) % 60),
-    minutes = Math.floor((duration / (1000 * 60)) % 60),
-    hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
-
-  hours = (hours < 10) ? "0" + hours : hours
-  minutes = (minutes < 10) ? "0" + minutes : minutes
-  seconds = (seconds < 10) ? "0" + seconds : seconds
-
-  return minutes + ` ${mssg.minute} ` + seconds + ` ${mssg.second}` 
-}
-function pickRandom(list) {
-  return list[Math.floor(list.length * Math.random())]
-}
+export default handler;
+  
