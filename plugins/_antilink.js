54dd648d23214a57d3c1a9951676dfa3d966d85f
Kennn <111826360+Principiante12344@users.noreply.github.com>
2024-04-07 14:15:38 -0500

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,2 +1,29 @@
-const linkRegex=/chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i;export async function before(t,{conn:a,isAdmin:n,isBotAdmin:o}){if(t.isBaileys&&t.fromMe)return!0;if(!t.isGroup)return!1;const e=global.db.data.chats[t.chat],s=global.db.data.settings[this.user.jid]||{},r=linkRegex.exec(t.text);if(e.antiLink&&r&&!n){const n=`https://chat.whatsapp.com/${await this.groupInviteCode(t.chat)}`;if(o&&t.text.includes(n))return!0;await a.reply(t.chat,"*≡ Enlace Detectado*\n\nNo permitimos enlaces de otros grupos \nlo siento *@${t.sender.split('@')[0]}*  serás expulsado del grupo ${o?'':'\n\nNo soy admin así que no te puedo expulsar :"v'}`,null,{mentions:[t.sender]});if(o&&e.antiLink){await a.sendMessage(t.chat,{delete:t.key}),await a.groupParticipantsUpdate(t.chat,[t.sender],'remove')}else if(!e.antiLink)return!1}return!0}
-    
+const linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i;
+
+export async function before(m, { conn, isAdmin, isBotAdmin }) {
+    if (m.isBaileys && m.fromMe) return true;
+    if (!m.isGroup) return false;
+
+    const chat = global.db.data.chats[m.chat];
+    const bot = global.db.data.settings[this.user.jid] || {};
+    const isGroupLink = linkRegex.exec(m.text);
+
+    if (chat.antiLink && isGroupLink && !isAdmin) {
+        const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`;
+        if (isBotAdmin && m.text.includes(linkThisGroup)) return true;
+
+        await conn.reply(m.chat, `*≡ Enlace Detectado*
+            
+No permitimos enlaces de otros grupos 
+lo siento *@${m.sender.split('@')[0]}*  serás expulsado del grupo ${isBotAdmin ? '' : '\n\nNo soy admin así que no te puedo expulsar :"v'}`, null, { mentions: [m.sender] });
+
+        if (isBotAdmin && chat.antiLink) {
+            await conn.sendMessage(m.chat, { delete: m.key });
+            await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
+        } else if (!chat.antiLink) {
+            return false;
+        }
+    }
+
+    return true;
+}
