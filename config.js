d318241c2c4a502479953aa56238f0767a9dbac6
Kennn <111826360+Principiante12344@users.noreply.github.com>
2024-04-07 23:43:56 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,46 +1,55 @@
+import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
-import { fileURLToPath } from 'url'
+import { fileURLToPath } from 'url' 
 
 global.owner = [
   ['51907364682', 'kenn', true],
   ['']
-]
+] //Numeros de owner 
 
-global.mods = ['']
-global.prems = ['51907364682', '51907364682']
-global.APIs = {
+global.mods = [''] 
+global.prems = ['', '']
+global.APIs = { // API Prefix
+  // name: 'https://website' 
   nrtm: 'https://fg-nrtm.ddns.net',
   fgmods: 'https://api.fgmods.xyz'
 }
-global.APIKeys = {
-  'https://api.fgmods.xyz': 'dEBWvxCY'
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://api.fgmods.xyz': 'dEBWvxCY' //--- 100 de límite diario --- Regístrese en https://api.fgmods.xyz/
 }
 
-global.packname = 'ONYX'
-global.author = 'KENN'
+// Sticker WM
+global.packname = 'ONYX' 
+global.author = 'KENN' 
 
+//--info FG
 global.botName = 'ONYX'
-global.fgig = 'https://whatsapp.com/channel/0029VaDajsjLI8YRnDjT2y1v'
-global.fgsc = 'https://whatsapp.com/channel/0029VaDajsjLI8YRnDjT2y1v'
+global.fgig = 'https://whatsapp.com/channel/0029VaDajsjLI8YRnDjT2y1v' 
+global.fgsc = 'https://whatsapp.com/channel/0029VaDajsjLI8YRnDjT2y1v' 
 global.fgyt = 'https://whatsapp.com/channel/0029VaDajsjLI8YRnDjT2y1v'
 global.fgpyp = 'https://whatsapp.com/channel/0029VaDajsjLI8YRnDjT2y1v'
-global.fglog = 'https://telegra.ph/file/5e10f68fb0c2801a2612d.jpg'
+global.fglog = 'https://telegra.ph/file/5e10f68fb0c2801a2612d.jpg' 
 
+//--- Grupos WA
 global.fgcanal = 'https://whatsapp.com/channel/0029VaDajsjLI8YRnDjT2y1v'
 global.bgp = 'https://whatsapp.com/channel/0029VaDajsjLI8YRnDjT2y1v'
 global.bgp2 = 'https://whatsapp.com/channel/0029VaDajsjLI8YRnDjT2y1v'
-global.bgp3 = 'https://whatsapp.com/channel/0029VaDajsjLI8YRnDjT2y1v'
+global.bgp3 = 'https://whatsapp.com/channel/0029VaDajsjLI8YRnDjT2y1v' //--GP NSFW
 
 global.wait = '⌛'
 global.rwait = '⌛'
 global.dmoji = '🤭'
 global.done = '✅'
-global.error = '❌'
-global.xmoji = '🔥'
+global.error = '❌' 
+global.xmoji = '🔥' 
 
-global.multiplier = 69
-global.maxwarn = '2'
+global.multiplier = 69 
+global.maxwarn = '2' // máxima advertencias
 
 let file = fileURLToPath(import.meta.url)
-console.log(chalk.redBright("Update 'config.js'"))
-import(`${file}?update=${Date.now()}`)
+watchFile(file, () => {
+  unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  import(`${file}?update=${Date.now()}`)
+})
